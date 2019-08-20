e35221907ebc57dbfaf00d38fdf086093082f4cb
Bryan Szendel <bryan.szendel@gmail.com>
2019-08-20 14:52:41 -0600

    login page redirects to protected page upon successful login

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 import Login from './components/Login'
+import FriendList from './components/FriendList'
+import PrivateRoute from './components/PrivateRoute'
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <ul>
+        <ul className="navlinks">
           <li>
             <Link to="/login">Login</Link>
           </li>
@@ -17,6 +19,7 @@ function App() {
           </li>
         </ul>
         <Route path="/login" component={Login} />
+        <PrivateRoute exact path="/protected" component={FriendList} />
       </div>
     </Router>
   );
---
+++ friends/src/components/FriendList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FriendList = () => {
+  return (
+    <div>
+      <p>You've reached the protected page :)</p>
+    </div>
+  )
+}
+
+export default FriendList;
